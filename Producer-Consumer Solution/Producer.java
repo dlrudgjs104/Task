0394ae42f45632749f83e9667825e93698d9ddb8
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-03-13 12:21:00 +0900

    Add files via upload

---
+++ Producer-Consumer Solution/Producer.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Producer implements Runnable {
+    private Store store;
+    private String name;
+
+    public Producer(String name, Store store) {
+        this.name = name;
+        this.store = store;
+    }
+
+    @Override
+    public void run() {
+        store.enter(name);
+        while (true) {
+            try {
+                store.buy();
+                Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 10000));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.err.println("Thread Interrupted");
+            }
+        }
+    }
+}
