43ac713b417ebf8366c9997cfc03f948d4a30053
dlrudgjs104 <dlrudgjs104@naver.com>
2024-06-08 00:06:52 +0900

    ..

--- Spring-Boot-MVC/CustomerService/src/main/java/com/nhnacademy/customerservice/exception/UserLoginFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.customerservice.exception;
-
-public class UserLoginFailedException extends RuntimeException{
-    public UserLoginFailedException(String message) {
-        super(message);
-    }
-}
